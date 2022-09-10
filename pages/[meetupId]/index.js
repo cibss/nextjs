afc9677fd2b809749af6cba220101394bfa47a2f
cibss <cibss13@gmail.com>
2022-09-10 22:36:42 +0700

    fix build issue

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+function MeetupDetails() {
+  return (
+    <MeetupDetail
+      image="https://images.adsttc.com/media/images/5afc/3629/f197/cc4a/5700/0072/large_jpg/SEOUL_COFFEE_-_1.jpg?1526478348"
+      title="a"
+      address="a"
+      description="a"
+    />
+  );
+}
+
+export default MeetupDetails;
