cc2a7b62a387919dcab5f384107e7e6003afa907
cibss <cibss13@gmail.com>
2022-09-11 21:05:43 +0700

    setup api route with mongodb

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,10 +1,28 @@
 // Endpoint: /api/new-meetup
 
-function handler(req, res) {
-  if (req.method === 'POST') {
+import { MongoClient } from "mongodb";
+
+async function handler(req, res) {
+  if (req.method === "POST") {
     const data = req.body;
-    const { title, image, address, description } = data;
+
+    const client = await MongoClient.connect(
+      `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_KEY}@clusterpractice.vwsbpra.mongodb.net/meetups?retryWrites=true&w=majority`
+    );
+    const db = client.db();
+
+    const meetupCollection = db.collection('meetups');  
+    
+    const result = await meetupCollection.insertOne(data);
+    console.log(result);
+
+    client.close();
+
+    res.status(201).json({ message: 'Meetup inserted!' });
   }
 }
 
 export default handler;
+
+// Collection = Table
+// Document = Table entries
